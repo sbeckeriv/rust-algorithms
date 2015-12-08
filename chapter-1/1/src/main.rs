3646a7888a7809d040b6bedd41d9b996431c431d
Stephen Becker IV <github@deathbyescalator.com>
2015-12-07 19:11:04 -0800

    Examples

--- chapter-1/1/src/main.rs
+++ chapter-1/1/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
     println!("Hello, world!");
+    // code fragments page #21
+
+    //find the max of the array values
+    let array = vec![3,6,5];
+    let mut max =  array.get(0).unwrap();
+    for n in &array{
+        if n> max{
+            max = n;
+        }
+    }
+    println!("{:?}", max);
 }
