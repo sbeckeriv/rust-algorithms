982df6271646ba662682b9c5f4f4975d52b0a329
Stephen Becker IV <github@deathbyescalator.com>
2015-12-07 19:49:40 -0800

    second example.
    took way to long to figure out floats

--- chapter-1/1/src/main.rs
+++ chapter-1/1/src/main.rs
@@ -3,12 +3,26 @@ fn main() {
     // code fragments page #21
 
     //find the max of the array values
-    let array = vec![3,6,5];
+    let array: Vec<u32> = vec![3,6,5];
     let mut max =  array.get(0).unwrap();
     for n in &array{
         if n> max{
             max = n;
         }
     }
-    println!("{:?}", max);
+    println!("max: {:?}", max);
+
+    //Compute the average
+    let mut sum = 0 as u32;
+    let size = array.len() as u32;
+    for n in &array{
+        sum = sum + n;
+    }
+    match size{
+        0 => println!("divide error"),
+        _ => {
+            let avg:f64 = (sum as f64)/(size as f64);
+            println!("avg: {:?}", avg);
+        }
+    };
 }
