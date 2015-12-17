53029e074c55042214a8ca7a0a5801e687ef1203
Stephen Becker IV <github@deathbyescalator.com>
2015-12-16 19:36:20 -0800

    formatted

--- chapter-1/1/src/chap.rs
+++ chapter-1/1/src/chap.rs
@@ -1,27 +1,26 @@
-//page 23
-
-pub fn abs(x: i64) -> i64{
-    if x<0{
-        x*(0-1)
-    } else{
+// page 23
+pub fn abs(x: i64) -> i64 {
+    if x < 0 {
+        x * (0 - 1)
+    } else {
         x
     }
 }
 
-pub fn abs_float(x: f64) -> f64{
-    if x<0.0{
-        x*(0.0-1.0)
-    } else{
+pub fn abs_float(x: f64) -> f64 {
+    if x < 0.0 {
+        x * (0.0 - 1.0)
+    } else {
         x
     }
 }
 
 pub fn is_prime(n: u64) -> bool {
-    if n<2 {
+    if n < 2 {
         false
-    } else{
-        for count in 2..(n/2){
-            if n%count==0 {
+    } else {
+        for count in 2..(n / 2) {
+            if n % count == 0 {
                 return false;
             }
         }
@@ -30,15 +29,15 @@ pub fn is_prime(n: u64) -> bool {
 }
 
 use std::f64;
-pub fn sqrt(n: f64)-> f64{
-    if n<0.0 {
+pub fn sqrt(n: f64) -> f64 {
+    if n < 0.0 {
         f64::NAN
-    }else{
-        let mut t= n.clone();
+    } else {
+        let mut t = n.clone();
         let base = 10.0_f64;
         let err = base.powi(-15);
-        while (t-(n/t)).abs() > (err*t) {
-            t=(n/t+t)/2.0;
+        while (t - (n / t)).abs() > (err * t) {
+            t = (n / t + t) / 2.0;
         }
         t
     }
@@ -46,74 +45,74 @@ pub fn sqrt(n: f64)-> f64{
 
 
 pub fn hypo(a: f64, b: f64) -> f64 {
-    (a*a+b*b).sqrt()
+    (a * a + b * b).sqrt()
 }
 
-pub fn h(n: u64) -> f64{
+pub fn h(n: u64) -> f64 {
     let mut sum = 0.0;
-    for i in 1..n+1{
-        sum = sum+(1.0/i as f64)
+    for i in 1..n + 1 {
+        sum = sum + (1.0 / i as f64)
     }
     sum
 }
 
-//page #25. close enough.
-//I would return a option instead of a bool.
-pub fn rank(vec: &Vec<usize>, find: usize) -> bool{
+// page #25. close enough.
+// I would return a option instead of a bool.
+pub fn rank(vec: &Vec<usize>, find: usize) -> bool {
     let max = vec.len();
     rank_find(vec, find, 0, max)
 }
 
-fn rank_find(vec: &Vec<usize>, find: usize, min: usize, max: usize) -> bool{
-    let mid = min+((max-min)/2);
+fn rank_find(vec: &Vec<usize>, find: usize, min: usize, max: usize) -> bool {
+    let mid = min + ((max - min) / 2);
     let mid_value = vec[mid];
-    if (max-min)==1 {
+    if (max - min) == 1 {
         false
-    }else if mid_value == find {
+    } else if mid_value == find {
         true
-    }else if mid_value < find {
+    } else if mid_value < find {
         rank_find(vec, find, mid, max)
-    }else{
+    } else {
         rank_find(vec, find, min, mid)
     }
 }
-//page # 32
+// page # 32
 extern crate rand;
 use self::rand::Rng;
-pub fn uni1(a: u64, b: u64) -> u64{
-     a + (rand::random::<f64>()*(b-a) as f64) as u64
+pub fn uni1(a: u64, b: u64) -> u64 {
+    a + (rand::random::<f64>() * (b - a) as f64) as u64
 }
 
-pub fn uni2(a: u64) -> u64{
-     (rand::random::<f64>()*a as f64) as u64
+pub fn uni2(a: u64) -> u64 {
+    (rand::random::<f64>() * a as f64) as u64
 }
 
-pub fn discrete(a: u64){
-    //nah
+pub fn discrete(a: u64) {
+    // nah
 }
 
 // not the same as the book
-pub fn shuffle(a: &mut Vec<usize>){
+pub fn shuffle(a: &mut Vec<usize>) {
     let mut rng = rand::thread_rng();
     let max = a.len();
-    for i in 0..max{
-        let swapper = rng.gen::<usize>()%max;
-        a.swap(i,swapper);
+    for i in 0..max {
+        let swapper = rng.gen::<usize>() % max;
+        a.swap(i, swapper);
     }
 }
 
-//page 47
-pub fn rank_loop(vec: &Vec<usize>, key: usize) -> bool{
+// page 47
+pub fn rank_loop(vec: &Vec<usize>, key: usize) -> bool {
     let mut low = 0;
     let mut high = vec.len();
-    while (high-low) > 1 {
-        let mid_index = low+(high-low)/2;
+    while (high - low) > 1 {
+        let mid_index = low + (high - low) / 2;
         let mid = vec[mid_index];
-        if mid == key{
+        if mid == key {
             return true;
-        } else if mid < key{
+        } else if mid < key {
             low = mid_index;
-        }else{
+        } else {
             high = mid_index;
         }
     }
--- chapter-1/1/src/main.rs
+++ chapter-1/1/src/main.rs
@@ -10,98 +10,99 @@ fn main() {
     println!("Hello, world!");
     // code fragments page #21
 
-    //find the max of the array values
-    let array: Vec<u32> = vec![3,6,5];
-    let mut max =  array.get(0).unwrap();
-    for n in &array{
-        if n> max{
+    // find the max of the array values
+    let array: Vec<u32> = vec![3, 6, 5];
+    let mut max = array.get(0).unwrap();
+    for n in &array {
+        if n > max {
             max = n;
         }
     }
     println!("max: {:?}", max);
 
-    //Compute the average
+    // Compute the average
     let mut sum = 0;
     let size = array.len();
-    for n in &array{
+    for n in &array {
         sum = sum + n;
     }
-    match size{
+    match size {
         0 => println!("divide error"),
         _ => {
-            let avg = (sum as f64)/(size as f64);
+            let avg = (sum as f64) / (size as f64);
             println!("avg: {:?}", avg);
         }
     };
 
-    //copy to another arra
-    //let array2 = array.clone();
-    let mut array2: Vec<u32> =  Vec::with_capacity(array.len());
-    for n in &array{
+    // copy to another arra
+    // let array2 = array.clone();
+    let mut array2: Vec<u32> = Vec::with_capacity(array.len());
+    for n in &array {
         array2.push(n.clone())
     }
 
-    //revrse theelements within the array
+    // revrse theelements within the array
     let len = array2.len();
-    for n in (0..len/2){
-        array2.swap(n, len-n-1);
+    for n in (0..len / 2) {
+        array2.swap(n, len - n - 1);
     }
     // matrix matrix multiplication square matrices
 
-    let a = vec![vec![1,1], vec![2,2]];
-    let b = vec![vec![3,3], vec![4,4]];
-    let mut c = vec![[0,0], [0,0]];
-    for j in 0..2{
-        for l in 0..2{
-            for k in 0..2{
+    let a = vec![vec![1, 1], vec![2, 2]];
+    let b = vec![vec![3, 3], vec![4, 4]];
+    let mut c = vec![[0, 0], [0, 0]];
+    for j in 0..2 {
+        for l in 0..2 {
+            for k in 0..2 {
                 let new_value;
-                let value = a.get(j).unwrap().get(k).unwrap() *
-                    b.get(k).unwrap().get(l).unwrap();
-                { // its own scope for reading
+                let value = a.get(j).unwrap().get(k).unwrap() * b.get(k).unwrap().get(l).unwrap();
+                {
+                    // its own scope for reading
                     let current_value = c.get(j).unwrap().get(l);
-                    new_value = match current_value{
-                        Some(current) => {current+value}
-                        None => value
+                    new_value = match current_value {
+                        Some(current) => current + value,
+                        None => value,
                     };
                 }
-                c.get_mut(j).unwrap()[l]= new_value;
+                c.get_mut(j).unwrap()[l] = new_value;
             }
         }
     }
     println!("matrix {:?}", c);
-    let y=chap::abs(0-1);
+    let y = chap::abs(0 - 1);
     println!("abs {:?}", y);
 
-    let y=chap::abs_float(0.0-1.1);
+    let y = chap::abs_float(0.0 - 1.1);
     println!("abs {:?}", y);
-    for n in 2..24{
-        println!("{}: {:?}",n, chap::is_prime(n))
+    for n in 2..24 {
+        println!("{}: {:?}", n, chap::is_prime(n))
     }
     println!("sqrt {:?}", chap::sqrt(33434.4));
-    println!("hypo {:?}", chap::hypo(12.0,5.0));
+    println!("hypo {:?}", chap::hypo(12.0, 5.0));
     println!("h{:?}", chap::h(3));
-    let find_me = vec![1,2,3,4,5,6,7,8,9,10];
+    let find_me = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     println!("rank{:?}", chap::rank(&find_me, 8));
     println!("rank{:?}", chap::rank(&find_me, 18));
-    println!("uni1 {:?}", chap::uni1(3,16));
+    println!("uni1 {:?}", chap::uni1(3, 16));
     println!("uni2 {:?}", chap::uni2(16));
 
 
     println!("rank_loop {:?}", chap::rank_loop(&find_me, 8));
     println!("rank_loop {:?}", chap::rank_loop(&find_me, 18));
 
-    let mut shuffle_me = vec![1,2,3,4,5,6,7,8,9,10];
+    let mut shuffle_me = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     chap::shuffle(&mut shuffle_me);
     println!("shuffle {:?}", shuffle_me);
-    let mut args:Vec<String> = env::args().collect();
+    let mut args: Vec<String> = env::args().collect();
     args.remove(0);
-    let parse_args: Vec<u64> = args.iter().
-        map(|arg| arg.parse::<u64>().unwrap()).collect();
+    let parse_args: Vec<u64> = args.iter()
+                                   .map(|arg| arg.parse::<u64>().unwrap())
+                                   .collect();
     println!("args {:?}", parse_args);
     let mut rng = rand::thread_rng();
-    let between = Range::new(parse_args[1], parse_args[2]+1);
+    let between = Range::new(parse_args[1], parse_args[2] + 1);
     for i in 0..parse_args[0] {
-        println!("Rando {:?}: {:?}",i, between.ind_sample(&mut rng));
+        println!("Rando {:?}: {:?}", i, between.ind_sample(&mut rng));
     }
 
     let mut sum = 0;
@@ -110,18 +111,18 @@ fn main() {
     let stdin = io::stdin();
     for line in stdin.lock().lines() {
         let string = line.unwrap();
-        match string.parse::<i64>(){
+        match string.parse::<i64>() {
             Ok(num) => {
-                if num >= 0{
-                    count = 1+count;
+                if num >= 0 {
+                    count = 1 + count;
                     sum = sum + num;
-                    println!("Sum {:?} of {:?} numbers", sum,count);
-                }else{
-                    println!("Average {:?}", (sum as f64/count as f64) as f64);
+                    println!("Sum {:?} of {:?} numbers", sum, count);
+                } else {
+                    println!("Average {:?}", (sum as f64 / count as f64) as f64);
                     process::exit(1);
                 }
-            },
-            Err(_) =>{
+            }
+            Err(_) => {
                 println!("Could not process number");
             }
         }
