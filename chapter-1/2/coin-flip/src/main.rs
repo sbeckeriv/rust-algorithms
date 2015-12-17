9c7bf8ebe7825a6ccfcbfd0078d67c282a7cb83f
Stephen Becker IV <github@deathbyescalator.com>
2015-12-16 19:40:58 -0800

    Formatted
    added random

--- chapter-1/2/coin-flip/src/main.rs
+++ chapter-1/2/coin-flip/src/main.rs
@@ -1,22 +1,31 @@
-struct Counter{
+extern crate rand;
+struct Counter {
     name: String,
     count: i64,
 }
 
-impl Counter{
-    fn new(name: &str) -> Counter{
-        Counter{
+impl Counter {
+    fn new(name: &str) -> Counter {
+        Counter {
             name: name.to_string(),
-            count: 0
+            count: 0,
         }
     }
-    fn increment(&mut self) -> i64{
+    fn increment(&mut self) -> i64 {
         self.count = self.count + 1;
         self.count
     }
 }
 
+use rand::Rng;
 fn main() {
     let mut counter = Counter::new("heads");
-    println!("{:?}",counter.increment());
+    println!("{:?}", counter.increment());
+
+    let mut rng = rand::thread_rng();
+    if rng.gen() {
+        // random bool
+        println!("i32: {}, u32: {}", rng.gen::<i32>(), rng.gen::<u32>())
+    }
+
 }
