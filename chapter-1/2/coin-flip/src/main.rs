1fbcb68cb76d7795ce3e3313b0f51d84bc509bdf
Stephen Becker IV <github@deathbyescalator.com>
2015-12-16 23:51:23 -0800

    Flip example
    remove warnings

--- chapter-1/2/coin-flip/src/main.rs
+++ chapter-1/2/coin-flip/src/main.rs
@@ -3,7 +3,7 @@ extern crate rand;
 #[derive(Debug)]
 struct Counter {
     name: String,
-    count: i64,
+    pub count: i64,
 }
 
 impl Counter {
@@ -19,23 +19,36 @@ impl Counter {
     }
 }
 
-use rand::Rng;
 use rand::distributions::{IndependentSample, Range};
 fn main() {
+    let mut rng = rand::thread_rng();
+    //arg parsing in 1.1 code
+    let t=5;
+    let mut heads = Counter::new("heads");
+    let mut tails = Counter::new("tails");
+    let float_range = Range::new(0.0,1.0);
+    for _ in 0..t+1{
+        if float_range.ind_sample(&mut rng)>0.5{
+            heads.increment();
+        }else{
+            tails.increment();
+        }
+    }
+
+    println!("Heads {:?}", heads);
+    println!("Tails {:?}", tails);
+    println!("delta {:?}", (heads.count-tails.count).abs());
+
+
     let mut counters: Vec<Counter> = vec![Counter::new("1"),
                                           Counter::new("2"),
                                           Counter::new("3"),
                                           Counter::new("4"),
                                           Counter::new("5"),
                                           Counter::new("6")];
-    let random_door = Range::new(0, 6);
-
-    let mut rng = rand::thread_rng();
-    if rng.gen() {
-        println!("i32: {}", random_door.ind_sample(&mut rng));
-    }
-    for x in 0..500 {
-        counters[random_door.ind_sample(&mut rng)].increment();
+    let random_dice = Range::new(0, 6);
+    for _ in 0..500 {
+        counters[random_dice.ind_sample(&mut rng)].increment();
     }
     println!("{:?}", counters);
 }
