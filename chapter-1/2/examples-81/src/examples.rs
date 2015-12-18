59add0192e1957af15326514c4cb3eacdb19916a
Stephen Becker IV <github@deathbyescalator.com>
2015-12-17 19:31:56 -0800

    Sorted example

--- chapter-1/2/examples-81/src/examples.rs
+++ chapter-1/2/examples-81/src/examples.rs
@@ -8,3 +8,14 @@ pub fn is_palindrome(s: &str) -> bool {
     }
     true
 }
+use std::cmp::Ordering;
+pub fn is_sorted(s: &str) -> bool {
+    let string_iter: Vec<char> = s.chars().collect();
+    let s_len = s.len() - 1;
+    for i in 0..s_len {
+        if Ordering::Less != string_iter[i].cmp(&string_iter[i + 1]) {
+            return false;
+        }
+    }
+    true
+}
--- chapter-1/2/examples-81/src/main.rs
+++ chapter-1/2/examples-81/src/main.rs
@@ -3,4 +3,6 @@ mod examples;
 fn main() {
     println!("bob {}", examples::is_palindrome("bob"));
     println!("bobs {}", examples::is_palindrome("bobs"));
+    println!("sorted {}", examples::is_sorted("abs"));
+    println!("not sorted {}", examples::is_sorted("asb"));
 }
