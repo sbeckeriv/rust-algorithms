59fcc11ea04ecce471346410a4fff432f2d89abd
Stephen Becker IV <github@deathbyescalator.com>
2015-12-19 15:17:23 -0800

    The stdin is crossed
    Need to work out how to read the pipe.

---
+++ chapter-1/2/whitelist/src/main.rs
@@ -0,0 +1,31 @@
+mod whitelist;
+use std::io;
+use std::env;
+use std::path::Path;
+use std::io::prelude::*;
+use std::fs::File;
+
+fn main() {
+    let mut arguments: Vec<String> = env::args().collect();
+    let file_string = arguments.pop().unwrap();
+    let file = Path::new(&file_string);
+    let mut open_file = File::open(&file).unwrap();
+    let mut buffer = String::new();
+    open_file.read_to_string(&mut buffer).unwrap();
+    let mut num_array = buffer.lines().map(|num| num.parse::<u64>().unwrap()).collect();
+    let whitelist = whitelist::StaticSetOfInts::new(num_array);
+    let mut input = String::new();
+    match io::stdin().read_line(&mut input) {
+        Ok(_) => {
+            match input.parse::<u64>(){
+                Ok(num) =>{
+                    if !whitelist.contains(num){
+                        println!("{}", input);
+                    }
+                }
+                Err(num) => println!("Could not parse:{:?}", num)
+            }
+        }
+        Err(error) =>{},
+    }
+}
---
+++ chapter-1/2/whitelist/src/whitelist.rs
@@ -0,0 +1,19 @@
+pub struct StaticSetOfInts{
+    array_of_ints: Vec<u64>
+}
+
+impl StaticSetOfInts {
+    pub fn new(mut array: Vec<u64>) -> StaticSetOfInts{
+        array.sort();
+        return StaticSetOfInts {
+            array_of_ints: array
+        };
+    }
+    pub fn contains(&self, number: u64) -> bool{
+        match self.array_of_ints.binary_search(&number){
+            Ok(_)=> true,
+            Err(_)=> false
+        }
+    }
+}
+
