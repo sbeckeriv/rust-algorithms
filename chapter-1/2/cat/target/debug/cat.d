7ee133f5d6de73e0ac5630fc470518b2fe4d222f
Stephen Becker IV <github@deathbyescalator.com>
2015-12-19 14:29:51 -0800

    Read and write cat file

--- chapter-1/2/cat/target/debug/cat.d
+++
@@ -1,5 +0,0 @@
-/Users/becker/trash/rust-algorithms/chapter-1/2/cat/target/debug/cat.d: src/main.rs
-
-/Users/becker/trash/rust-algorithms/chapter-1/2/cat/target/debug/cat: src/main.rs
-
-src/main.rs:
