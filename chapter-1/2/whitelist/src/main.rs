04d8f1550fffeab5f264a0377844aafbad1e0667
Stephen Becker IV <github@deathbyescalator.com>
2015-12-19 15:39:24 -0800

    formatted

--- chapter-1/2/whitelist/src/main.rs
+++ chapter-1/2/whitelist/src/main.rs
@@ -6,26 +6,29 @@ use std::io::prelude::*;
 use std::fs::File;
 
 fn main() {
+    // is there a better way to do this?
     let mut arguments: Vec<String> = env::args().collect();
     let file_string = arguments.pop().unwrap();
+    // a better way to read a files content?
     let file = Path::new(&file_string);
     let mut open_file = File::open(&file).unwrap();
     let mut buffer = String::new();
     open_file.read_to_string(&mut buffer).unwrap();
     let mut num_array = buffer.lines().map(|num| num.parse::<u64>().unwrap()).collect();
     let whitelist = whitelist::StaticSetOfInts::new(num_array);
+
     let mut input = String::new();
     match io::stdin().read_line(&mut input) {
         Ok(_) => {
-            match input.parse::<u64>(){
-                Ok(num) =>{
-                    if !whitelist.contains(num){
+            match input.parse::<u64>() {
+                Ok(num) => {
+                    if !whitelist.contains(num) {
                         println!("{}", input);
                     }
                 }
-                Err(num) => println!("Could not parse:{:?}", num)
+                Err(num) => println!("Could not parse:{:?}", num),
             }
         }
-        Err(error) =>{},
+        Err(error) => {}
     }
 }
--- chapter-1/2/whitelist/src/whitelist.rs
+++ chapter-1/2/whitelist/src/whitelist.rs
@@ -1,19 +1,16 @@
-pub struct StaticSetOfInts{
-    array_of_ints: Vec<u64>
+pub struct StaticSetOfInts {
+    array_of_ints: Vec<u64>,
 }
 
 impl StaticSetOfInts {
-    pub fn new(mut array: Vec<u64>) -> StaticSetOfInts{
+    pub fn new(mut array: Vec<u64>) -> StaticSetOfInts {
         array.sort();
-        return StaticSetOfInts {
-            array_of_ints: array
-        };
+        return StaticSetOfInts { array_of_ints: array };
     }
-    pub fn contains(&self, number: u64) -> bool{
-        match self.array_of_ints.binary_search(&number){
-            Ok(_)=> true,
-            Err(_)=> false
+    pub fn contains(&self, number: u64) -> bool {
+        match self.array_of_ints.binary_search(&number) {
+            Ok(_) => true,
+            Err(_) => false,
         }
     }
 }
-
