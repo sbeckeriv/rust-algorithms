5388ec1511b46339ddc123c1ae8d773b096ed48f
Stephen Becker IV <github@deathbyescalator.com>
2015-12-28 13:48:27 -0800

    example as n^3

---
+++ chapter-1/4/3some/src/main.rs
@@ -0,0 +1,38 @@
+extern crate time;
+use std::env;
+use std::path::Path;
+use std::io::prelude::*;
+use std::fs::File;
+fn count(array: Vec<isize>) -> usize {
+    let mut count = 0;
+    let array_len = array.len();
+    for i in 0..array_len {
+        for f in i + 1..array_len {
+            for g in f + 1..array_len {
+                match array[i] + array[f] + array[g] {
+                    0 => count += 1,
+                    _ => {}
+                }
+            }
+        }
+    }
+    count
+}
+fn read_ints(file_string: String) -> Vec<isize> {
+    let file = Path::new(&file_string);
+    let mut open_file = File::open(file).unwrap();
+    let mut buffer = String::new();
+    open_file.read_to_string(&mut buffer).unwrap();
+    buffer.lines()
+          .map(|num| num.trim().parse::<isize>().unwrap())
+          .collect::<Vec<isize>>()
+}
+fn main() {
+    let mut arguments: Vec<String> = env::args().collect();
+    let file_string = arguments.pop().unwrap();
+
+    let start2 = time::now();
+    println!("{}, took: {:?}",
+             count(read_ints(file_string)),
+             time::now() - start2)
+}
