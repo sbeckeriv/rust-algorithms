89668f2f22e9a8f38eb102776f9d16ef614b2975
Stephen Becker IV <github@deathbyescalator.com>
2016-01-09 12:59:26 -0800

    Works but its in correct
    1000 numbers is taking way to long. i must have missed a condtion

---
+++ chapter-2/quick-sort-3-way/src/main.rs
@@ -0,0 +1,28 @@
+mod timer;
+mod sort;
+use std::env;
+use std::path::Path;
+use std::io::prelude::*;
+use std::fs::File;
+
+fn read_file_chars(file_string: String) -> Vec<isize> {
+    let file = Path::new(&file_string);
+    let mut open_file = File::open(file).unwrap();
+    let mut buffer = String::new();
+    open_file.read_to_string(&mut buffer).unwrap();
+    buffer.split_whitespace().map(|num| num.parse::<isize>().unwrap()).collect::<Vec<isize>>()
+}
+
+fn main() {
+    let mut arguments: Vec<String> = env::args().collect();
+    let file_string = arguments.pop().unwrap();
+    let mut vec = read_file_chars(file_string);
+    let len = vec.len()-1;
+    let mut sorter = sort::Algo::new(&mut vec);
+    let spent = timer::record(|| {
+        sorter.sort(0, len);
+    });
+    println!("{:?}", sorter);
+    println!("{:?}", sorter.is_sorted());
+    println!("{:?}", spent);
+}
---
+++ chapter-2/quick-sort-3-way/src/sort.rs
@@ -0,0 +1,56 @@
+use std::cmp::Ordering;
+#[derive(Debug)]
+pub struct Algo<'a> {
+    vec: &'a mut Vec<isize>,
+}
+
+impl<'a> Algo<'a> {
+    pub fn new(vec: &'a mut Vec<isize>) -> Self {
+        Algo { vec: vec }
+    }
+    pub fn len(&self) -> usize {
+        self.vec.len()
+    }
+    pub fn is_sorted(&self) -> bool {
+        let mut sorted = true;
+        for i in 0..self.len() - 1 {
+            if self.vec[i] > self.vec[i + 1] {
+                sorted = false;
+            }
+        }
+        sorted
+    }
+    pub fn sort(&mut self, lo: usize, hi: usize) {
+        if hi <= lo {
+            return;
+        }
+        let mut lt = lo;
+        let mut gt = hi;
+        let mut i = lo + 1;
+        let mut v = self.vec[lo];
+        while i <= gt {
+            let compared = self.vec[i].cmp(&v);
+            match compared {
+                Ordering::Less => {
+                    self.vec.swap(lt, i);
+                    lt += 1;
+                    i += 1;
+                }
+                Ordering::Greater => {
+                    self.vec.swap(i, gt);
+                    gt -= 1;
+                }
+                _ => {
+                    i += 1;
+                }
+            }
+            if lt > 0 {
+                self.sort(lo, lt - 1);
+            } else {
+                self.sort(lo, 0);
+            }
+
+            self.sort(gt + 1, hi);
+        }
+    }
+}
