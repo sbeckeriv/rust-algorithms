30477244e0d8a60dc30b5ca006ee59e17763abb1
Stephen Becker IV <github@deathbyescalator.com>
2016-01-11 20:29:29 -0800

    Progess

--- chapter-2/top_m/src/pq.rs
+++ chapter-2/top_m/src/pq.rs
@@ -1,32 +1,32 @@
-
 #[derive(Debug)]
 pub struct PQAlgo<'a, T: 'a> {
     vec: &'a mut Vec<T>,
     count: usize,
 }
-impl PQAlgoArray for PQAlgo {}
 
-pub trait <'a, T: Ord> PQAlgoArray<'a, T> {
-    pub fn new(vec: &'a mut Vec<T>) -> Self {
+impl<'a, T: Ord> PQAlgoArray<'a, T> for PQAlgo<'a, T> {}
+
+pub trait PQAlgoArray<'a, T: Ord> {
+    fn new(vec: &'a mut Vec<T>) -> Self {
         PQAlgo {
             vec: vec,
             count: 0,
         }
     }
-    pub fn insert(&mut self, item: T) {
+    fn insert(&mut self, item: T) {
         self.vec.push(item);
         self.vec.sort();
     }
-    pub fn max(&self) -> &T {
+    fn max(&self) -> &T {
         &self.vec[0]
     }
-    pub fn del_max(&mut self) -> T {
+    fn del_max(&mut self) -> T {
         self.vec.pop().unwrap()
     }
-    pub fn is_empty(&self) -> bool {
+    fn is_empty(&self) -> bool {
         self.vec.len() > 0
     }
-    pub fn size(&self) -> usize {
+    fn size(&self) -> usize {
         self.vec.len()
     }
 }
