d8f3104f6773d4137362ee0cebbc397832b1b130
Stephen Becker IV <github@deathbyescalator.com>
2016-01-12 19:44:26 -0800

    Heap

---
+++ chapter-2/heap_queue/src/sort.rs
@@ -0,0 +1,25 @@
+#[derive(Debug)]
+pub struct Algo<'a, T: 'a> {
+    vec: &'a mut Vec<T>,
+    count: usize,
+}
+impl<'a, T: Ord> Algo<'a, T> {
+    pub fn new(vec: &'a mut Vec<T>) -> Self {
+        Algo {
+            vec: vec,
+            count: 0,
+        }
+    }
+    pub fn len(&self) -> usize {
+        3
+    }
+    pub fn is_sorted(&self) -> bool {
+        let mut sorted = true;
+        for i in 0..self.len() {
+            if self.vec[i] > self.vec[i + 1] {
+                sorted = false;
+            }
+        }
+        sorted
+    }
+}
